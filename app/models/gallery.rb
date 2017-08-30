30037e02e25aed8571469160d167c24eea8f27bd
Fred Kim <yhk1038@hanmail.net>
2017-08-30 15:12:28 +0900

    relations

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,4 +1,5 @@
 class Gallery < ApplicationRecord
     has_many :user_galleries, dependent: :destroy
+    has_many :users, through: :user_galleries
     has_many :photos, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
     has_many :user_galleries, dependent: :destroy
+    has_many :galleries, through: :user_galleries
 end
