1896af15bfc065cc584e9bae820a51beee4b530c
Fred Kim <yhk1038@hanmail.net>
2017-09-05 20:37:58 +0900

    delete root

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :update, :destroy]
 
-    # root
-    def rooting
-        render json: {server: false}
-    end
 
     # GET /connection
     def connection
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
     post '/user_galleries/sync', to: 'user_galleries#update'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-    root 'users#rooting'
+    # root 'users#rooting'
 end
