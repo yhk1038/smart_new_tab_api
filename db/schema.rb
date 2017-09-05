343999077d832a8b676c389f7bdf12ce78bec6e6
Fred Kim <yhk1038@hanmail.net>
2017-09-05 22:41:44 +0900

    add colum email to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170827085541) do
+ActiveRecord::Schema.define(version: 20170905134014) do
 
   create_table "galleries", force: :cascade do |t|
     t.string "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20170827085541) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
